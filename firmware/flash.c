fa4e99e0ae4dc7b8259813f082a18aa3c8916fa0
Norbert Braun <n.braun@ikp.uni-koeln.de>
2013-04-10 23:10:17 +0200

    normalize coding convention

--- firmware/flash.c
+++ firmware/flash.c
@@ -3,22 +3,27 @@
 
 #define KPCB_FLASH_BASE_ADDR 0x080FFFFF - 0x00002000
 
-void pcb_flash_setup(void) {
-	// set waitstates
-	flash_set_ws(FLASH_ACR_LATENCY_5WS);
-	flash_unlock();
+void pcb_flash_setup(void)
+{
+    // set waitstates
+    flash_set_ws(FLASH_ACR_LATENCY_5WS);
+    flash_unlock();
 }
 
-void pcb_flash_store(uint16_t key, uint8_t byte) {
-	if (key > 0xFFF) {
-		return;
-	}
-	flash_program_byte(KPCB_FLASH_BASE_ADDR + key, byte, FLASH_CR_PROGRAM_X8);
+void pcb_flash_store(uint16_t key, uint8_t byte)
+{
+    if (key > 0xFFF) {
+        return;
+    }
+    
+    flash_program_byte(KPCB_FLASH_BASE_ADDR + key, byte, FLASH_CR_PROGRAM_X8);
 }
 
-uint8_t pcb_flash_restore(uint16_t key) {
-	if (key > 0xFFF) {
-		return 0x0;
-	}
-	return MMIO8(KPCB_FLASH_BASE_ADDR + key);
+uint8_t pcb_flash_restore(uint16_t key)
+{
+    if (key > 0xFFF) {
+        return 0x0;
+    }
+    
+    return MMIO8(KPCB_FLASH_BASE_ADDR + key);
 }
--- firmware/stepper.c
+++ firmware/stepper.c
@@ -11,23 +11,25 @@ const int states[][4] = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
 static int current_state = 0;
 
 
-void stepper_setup(void) {
-	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPDEN);
-
-	/* Set GPIO12-15 (in GPIO port D) to 'output push-pull'. */
-	gpio_mode_setup(STEPPER_PORT, GPIO_MODE_OUTPUT,
-	                GPIO_PUPD_NONE, STEPPER_BLACK_PIN | STEPPER_BROWN_PIN | STEPPER_ORANGE_PIN | STEPPER_YELLOW_PIN);
-	
-	/* Switch off all LEDs. */
-	gpio_clear(STEPPER_PORT, STEPPER_BLACK_PIN | STEPPER_BROWN_PIN | STEPPER_ORANGE_PIN | STEPPER_YELLOW_PIN);
+void stepper_setup(void)
+{
+    rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPDEN);
+    
+    /* Set GPIO12-15 (in GPIO port D) to 'output push-pull'. */
+    gpio_mode_setup(STEPPER_PORT, GPIO_MODE_OUTPUT,
+                    GPIO_PUPD_NONE, STEPPER_BLACK_PIN | STEPPER_BROWN_PIN | STEPPER_ORANGE_PIN | STEPPER_YELLOW_PIN);
+    
+    /* Switch off all LEDs. */
+    gpio_clear(STEPPER_PORT, STEPPER_BLACK_PIN | STEPPER_BROWN_PIN | STEPPER_ORANGE_PIN | STEPPER_YELLOW_PIN);
 }
 
-void stepper_step(int dir) {
-	int inc = dir ? 1 : -1;
-	current_state = ((current_state + inc) + kNUMBER_OF_STATES) % kNUMBER_OF_STATES;
-	
-	states[current_state][0] ? gpio_set(STEPPER_PORT, STEPPER_BROWN_PIN) : gpio_clear(STEPPER_PORT, STEPPER_BROWN_PIN);	
-	states[current_state][2] ? gpio_set(STEPPER_PORT, STEPPER_BLACK_PIN) : gpio_clear(STEPPER_PORT, STEPPER_BLACK_PIN);
-	states[current_state][1] ? gpio_set(STEPPER_PORT, STEPPER_YELLOW_PIN): gpio_clear(STEPPER_PORT, STEPPER_YELLOW_PIN);
-	states[current_state][3] ? gpio_set(STEPPER_PORT, STEPPER_ORANGE_PIN): gpio_clear(STEPPER_PORT, STEPPER_ORANGE_PIN);
+void stepper_step(int dir)
+{
+    int inc = dir ? 1 : -1;
+    current_state = ((current_state + inc) + kNUMBER_OF_STATES) % kNUMBER_OF_STATES;
+    
+    states[current_state][0] ? gpio_set(STEPPER_PORT, STEPPER_BROWN_PIN) : gpio_clear(STEPPER_PORT, STEPPER_BROWN_PIN);	
+    states[current_state][2] ? gpio_set(STEPPER_PORT, STEPPER_BLACK_PIN) : gpio_clear(STEPPER_PORT, STEPPER_BLACK_PIN);
+    states[current_state][1] ? gpio_set(STEPPER_PORT, STEPPER_YELLOW_PIN): gpio_clear(STEPPER_PORT, STEPPER_YELLOW_PIN);
+    states[current_state][3] ? gpio_set(STEPPER_PORT, STEPPER_ORANGE_PIN): gpio_clear(STEPPER_PORT, STEPPER_ORANGE_PIN);
 }
